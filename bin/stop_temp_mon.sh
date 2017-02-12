9d479e2ff77b5bd16742c75ffa371e4e32c8961f
mabood <mgabood8@gmail.com>
2017-02-11 16:34:54 -0800

    stop script

---
+++ bin/stop_temp_mon.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd /home/pi/dev/PiTempMon/bin
+
+ps | grep -f TMON_PID
+RUNNING=$?
+read -r PID <TMON_PID
+
+if [ $RUNNING -eq 0 ]; then
+   echo "Terminating TempMon..."
+   kill -s SIGKILL $PID
+else
+   echo "TempMon not running"
+fi
